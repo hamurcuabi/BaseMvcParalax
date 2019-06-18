6280ac1c4626f6ec448aa8b26d6cc8cbe3018c80
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2019-06-18 16:00:43 +0300

    Add project files.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BaseMvcParalax.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+
+            bundles.Add(new ScriptBundle("~/scripts/all").Include(
+                    "~/Template/vendor/jquery/jquery.min.js",
+                "~/Template/vendor/jquery.appear/jquery.appear.min.js",
+                "~/Template/vendor/jquery.easing/jquery.easing.min.js",
+                "~/Template/vendor/jquery.cookie/jquery.cookie.min.js",
+                "~/Template/vendor/popper/umd/popper.min.js",
+                "~/Template/vendor/bootstrap/js/bootstrap.min.js",
+                "~/Template/vendor/common/common.min.js",
+                "~/Template/vendor/jquery.validation/jquery.validate.min.js",
+                "~/Template/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js",
+                "~/Template/vendor/jquery.gmap/jquery.gmap.min.js",
+                "~/Template/vendor/jquery.lazyload/jquery.lazyload.min.js",
+                "~/Template/vendor/isotope/jquery.isotope.min.js",
+                "~/Template/vendor/owl.carousel/owl.carousel.min.js",
+                "~/Template/vendor/magnific-popup/jquery.magnific-popup.min.js",
+                "~/Template/vendor/vide/jquery.vide.min.js",
+                "~/Template/vendor/vivus/vivus.min.js",
+                "~/Template/js/theme.js", 
+                "~/Template/vendor/rs-plugin/js/jquery.themepunch.tools.min.js",
+                "~/Template/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js",
+                "~/Template/js/views/view.contact.js",
+                 "~/Template/js/custom.js",
+                 "~/Template/js/theme.init.js",
+                 "~/Template/js/examples/examples.portfolio.js",
+                 "~/Template/master/analytics/analytics.js"
+
+               )
+            );
+
+
+            bundles.Add(new StyleBundle("~/theme/css").Include(
+                "~/Template/css/theme.css",
+                "~/Template/css/theme-elements.css",
+                "~/Template/css/theme-blog.css",
+                "~/Template/css/theme-shop.css")
+            );
+
+
+            bundles.Add(new StyleBundle("~/vendor/css").Include(
+                "~/Template/vendor/bootstrap/css/bootstrap.min.css",
+                "~/Template/vendor/fontawesome-free/css/all.min.css",
+                "~/Template/vendor/animate/animate.min.css",
+                "~/Template/vendor/simple-line-icons/css/simple-line-icons.min.css",
+            "~/Template/vendor/owl.carousel/assets/owl.carousel.min.css",
+                "~/Template/vendor/owl.carousel/assets/owl.theme.default.min.css",
+                "~/Template/vendor/magnific-popup/magnific-popup.min.css",
+                "~/Template/css/theme.css",
+                "~/Template/css/theme-elements.css",
+                "~/Template/css/theme-blog.css",
+                "~/Template/css/theme-shop.css",
+                "~/Template/vendor/rs-plugin/css/settings.css",
+                "~/Template/vendor/rs-plugin/css/layers.css",
+                "~/Template/vendor/rs-plugin/css/navigation.css",
+                "~/Template/css/skins/default.css",
+                "~/Template/css/custom.css"
+                ));
+
+            bundles.Add(new StyleBundle("~/current/css").Include(
+                "~/Template/vendor/rs-plugin/css/settings.css",
+                "~/Template/vendor/rs-plugin/css/layers.css",
+                "~/Template/vendor/rs-plugin/css/navigation.css",
+                "~/Template/css/skins/default.css",
+                "~/Template/css/custom.css"
+                ));
+
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/SliderTextsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BaseMvcParalax;
+
+namespace BaseMvcParalax.Areas.Admin.Controllers
+{
+    public class SliderTextsController : Controller
+    {
+        private BaseMvcParalaxEntities db = new BaseMvcParalaxEntities();
+
+        // GET: Admin/SliderTexts
+        public ActionResult Index()
+        {
+            return View(db.SliderTexts.ToList());
+        }
+
+        // GET: Admin/SliderTexts/Details/5
+        public ActionResult Details(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SliderText sliderText = db.SliderTexts.Find(id);
+            if (sliderText == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sliderText);
+        }
+
+        // GET: Admin/SliderTexts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/SliderTexts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,MainText,Author,Sort,Visible")] SliderText sliderText)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SliderTexts.Add(sliderText);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sliderText);
+        }
+
+        // GET: Admin/SliderTexts/Edit/5
+        public ActionResult Edit(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SliderText sliderText = db.SliderTexts.Find(id);
+            if (sliderText == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sliderText);
+        }
+
+        // POST: Admin/SliderTexts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,MainText,Author,Sort,Visible")] SliderText sliderText)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sliderText).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sliderText);
+        }
+
+        // GET: Admin/SliderTexts/Delete/5
+        public ActionResult Delete(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SliderText sliderText = db.SliderTexts.Find(id);
+            if (sliderText == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sliderText);
+        }
+
+        // POST: Admin/SliderTexts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(byte id)
+        {
+            SliderText sliderText = db.SliderTexts.Find(id);
+            db.SliderTexts.Remove(sliderText);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
