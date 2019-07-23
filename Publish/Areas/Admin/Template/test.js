ef23ad7eabc8291d8cdb9020a39b34c411069d65
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2019-07-23 09:36:43 +0300

    dsadsa

---
+++ Publish/Areas/Admin/Template/test.js
@@ -0,0 +1,25 @@
+﻿
+jQuery(function () {
+
+    jQuery("#test").on("click",
+        ".btnTest",
+        function () {
+            bootbox.confirm({
+                message: "This is a confirm with custom button text and color! Do you like it?",
+                buttons: {
+                    confirm: {
+                        label: 'Yes',
+                        className: 'btn-success'
+                    },
+                    cancel: {
+                        label: 'No',
+                        className: 'btn-danger'
+                    }
+                },
+                callback: function (result) {
+                    console.log('This was logged in the callback: ' + result);
+                }
+            });
+
+        });
+});
---
+++ Publish/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaseMvcParalax.Global" Language="C#" %>
